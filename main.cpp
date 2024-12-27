498afe23e2705280e8c083d4ca1431ea574d8a30
Your Name <sanreaksmey01@gmail.com>
2024-12-27 14:11:30 +0700

    Sum

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number1,number2;
+    cout<<"Enter number 1: ";cin>>number1;
+    cout<<"Enter number 2: ";cin>>number2;
+
+    int result = number1 + number2;
+
+    cout<<"The sum of"<<number1<<"and "<<number2<<" is "<<result<<endl;
+
+    return 0;
+}
